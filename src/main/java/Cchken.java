894bec517cb4f9a0ca5084e30cdd9553e41ea5f8
IntensioT <alexander0aki@gmail.com>
2021-10-27 21:22:35 +0300

    dd

--- src/main/java/Cchken.java
+++ src/main/java/Cchken.java
@@ -1,5 +1,5 @@
 public class Cchken {
     public static void main(String[] args) {
-        System.out.println("Hekwokd21");
+        System.out.println("Hello world21");
     }
 }
--- src/test/java/CchkenTest.java
+++ src/test/java/CchkenTest.java
@@ -4,6 +4,9 @@ public class CchkenTest {
 
     @org.junit.Test
     public void main() {
+        /**
+         * one line
+         */
         String test="Helloworld";
         assertEquals("Helloworld",test);
     }
